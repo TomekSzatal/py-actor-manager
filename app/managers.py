5708cb5f68f681d1db8928c63596b0accace1390
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-12-23 17:27:35 +0100

    q

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,64 @@
 import sqlite3
+from models import Actor
 
-from app.models import Actor
 
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS {self.table_name} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name TEXT NOT NULL,
+                last_name TEXT NOT NULL
+            )
+            """
+        )
 
-# add manager here
+        self.conn.commit()
+
+    def create(self, first_name: str, last_name: str) -> int:
+        self.cursor.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            (first_name, last_name),
+        )
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    def all(self) -> list[Actor]:
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        rows = self.cursor.fetchall()
+        actors = [
+            Actor(
+                id=row["id"],
+                first_name=row["first_name"],
+                last_name=row["last_name"],
+            )
+            for row in rows
+        ]
+        return actors
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> int:
+        self.cursor.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ?, last_name = ? "
+            "WHERE id = ?",
+            (new_first_name, new_last_name, pk),
+        )
+        self.conn.commit()
+        return self.cursor.rowcount
+
+    def delete(self, pk: int) -> int:
+        self.cursor.execute(
+            f"DELETE FROM {self.table_name} WHERE id = ?",
+            (pk,),
+        )
+        self.conn.commit()
+        return self.cursor.rowcount
+
+    def __del__(self) -> None:
+        self.conn.close()
